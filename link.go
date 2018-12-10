bcbd6471185e4f91b5e43c82bd27ba7ba9129e9c
Alex Thompson <awt1088@gmail.com>
2018-12-10 10:06:28 -0800

    structs for users and links

---
+++ link.go
@@ -0,0 +1,8 @@
+package main
+
+type Link struct {
+	LinkUrl string
+	CreateDate string
+	Description string
+	Thumbnail string
+}
---
+++ user.go
@@ -0,0 +1,8 @@
+package main
+
+type User struct {
+	Id        string
+	Email     string
+	FirstName string
+	LastName  string
+}
