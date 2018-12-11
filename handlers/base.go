fb6ca920d90343ffef7a79a30393b80b5c36172a
Alex Thompson <awt1088@gmail.com>
2018-12-10 20:36:19 -0800

    added infrastructure for sign in with google, although not currently working

---
+++ handlers/base.go
@@ -0,0 +1,13 @@
+package handlers
+
+//func New() http.Handler {
+//	mux := http.NewServeMux()
+//	// Root
+//	mux.Handle("/", http.FileServer(http.Dir("templates/")))
+//
+//	// OauthGoogle
+//	mux.HandleFunc("/auth/google/login", oauthGoogleLogin)
+//	mux.HandleFunc("/auth/google/callback", oauthGoogleCallback)
+//
+//	return mux
+//}
