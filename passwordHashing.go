f6dbfc1c013c80842410c8ae3c4640aa7bc8165f
Alex Thompson <awt1088@gmail.com>
2018-12-10 22:30:01 -0800

    func to hash a password and return the binary for the hashedpassword and salt

---
+++ passwordHashing.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/rand"
+	"golang.org/x/crypto/scrypt"
+	"io"
+	"log"
+)
+
+const  (
+	pwSaltBytes = 32
+	pwHashBytes = 64
+)
+
+func hashAndSalt(password string) ([]byte, []byte) {
+	salt := make([]byte, pwSaltBytes)
+	_, err := io.ReadFull(rand.Reader, salt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hash, err := scrypt.Key([]byte(password), salt, 1<<14, 8, 1, pwHashBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return hash, salt
+}
